df40c378a8e2c8005db782015048607cff700f94
nikhilverma163 <nikhilvermaanilsdt123@gmail.com>
2025-08-28 00:26:40 +0530

    Add files via upload

---
+++ code8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum;
+
+   
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+  
+    sum = (n * (n + 1)) / 2;
+
+   
+    printf("Sum of the first %d natural numbers = %d\n", n, sum);
+
+    return 0;
+}
