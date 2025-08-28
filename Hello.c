43c1f410637c6c656105743ff7ecd6f318fa1945
nikhilverma163 <nikhilvermaanilsdt123@gmail.com>
2025-08-28 13:35:51 +0530

    Update Hello.c

--- Hello.c
+++ Hello.c
@@ -3,17 +3,17 @@
 int main() {
     int num1, num2, sum;
 
-    // Input two numbers
+    
     printf("Enter first number: ");
     scanf("%d", &num1);
 
     printf("Enter second number: ");
     scanf("%d", &num2);
 
-    // Calculate sum
+    
     sum = num1 + num2;
 
-    // Display result
+    
     printf("The sum is: %d\n", sum);
 
     return 0;
