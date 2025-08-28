e775352ff0ea10b55a74bd14cda7af87c2fdae43
nikhilverma163 <nikhilvermaanilsdt123@gmail.com>
2025-08-28 13:37:03 +0530

    Update code13.c

--- code13.c
+++ code13.c
@@ -7,15 +7,7 @@ int main() {
     printf("Enter a year: ");
     scanf("%d", &year);
 
-    
-    if (year % 400 == 0) {
-        printf("%d is a Leap Year\n", year);
-    }
-    else if (year % 100 == 0) {
-        printf("%d is Not a Leap Year\n", year);
-    }
-    else if (year % 4 == 0) {
-        printf("%d is a Leap Year\n", year);
+    if  printf("%d is a Leap Year\n", year);
     }
     else {
         printf("%d is Not a Leap Year\n", year);
